e5c4b3461e93f3df3905d48cb0f4794d5e3c58c1
edreeseg <edward.reeseg@gmail.com>
2019-08-08 08:57:57 -0400

    Sieve function finished

--- src/16_prime.py
+++ src/16_prime.py
@@ -1,18 +1,17 @@
 def sieve_of_eratosthenes(limit):
     p = 2
     l = [False, False] + [True for n in range(2, limit+1)]
-    print(l)
-    isFinished = False
-    while(isFinished == False):
-        isFinished = True
+    finished = False
+    while(not finished):
+        finished = True
         i = p**2
         while(i <= limit):
-            isFinished = False
+            finished = False
             l[i] = False
             i += p
-        for i, n in enumerate(l, p + 1):
-            if (n == True):
-                p = i
+        for index, value in enumerate(l[p+1:], p+1):
+            if (value == True):
+                p = index
                 break
-    print(l)
-sieve_of_eratosthenes(20)
+    return [i for i, prime in enumerate(l) if prime]
+print(sieve_of_eratosthenes(100000))
